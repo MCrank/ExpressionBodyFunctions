9c77f1c8b4af9721660a905d42a0292d8e21dc6c
MCrank <marco_mcse@hotmail.com>
2019-03-08 03:58:02 -0600

    Add project files.

---
+++ ExpressionBodyFunctions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpressionBodyFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
